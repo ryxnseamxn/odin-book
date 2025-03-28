0e4e7b58e18451fc7e5bfa0b9ddd86b48f78d2ee
Ryan Seaman <ryan.l.seaman@gmail.com>
2025-03-27 22:29:20 -0500

    ORM work
    added code to configure typeORM instance and connect on server start

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const reflect = require("reflect-metadata")
+const reflect = require("reflect-metadata");
+const dataSource = require("./db/config");
 
 const app = express();
 
-app.listen(3001, () => {
+app.listen(3001, async () => {
+    await dataSource.initialize();
     console.log("app listening on port 3000!")
 });
